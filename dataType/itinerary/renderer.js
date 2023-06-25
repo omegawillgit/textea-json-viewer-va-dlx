9d342592a77be6cc8e1694fa26884f8d51936e53
omegawillgit <40714524+omegawillgit@users.noreply.github.com>
2023-06-26 09:11:53 +1000

    1 file modified

--- dataType/itinerary/renderer.js
+++ dataType/itinerary/renderer.js
@@ -1,6 +1,10 @@
 export const itineraryRenderer = (props) => {
   return (
     <>
+      {
+        props.value.originDestination.at(0)?.flightSegment.at(0)
+          ?.marketingAirlineRef.id
+      }{' '}
       {props.value.originDestination.at(0)?.flightSegment.at(0)?.flightNumber}
     </>
   );
